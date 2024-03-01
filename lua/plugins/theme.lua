0bb8a882f130653986f1bbda6f5787652a91fabb
meer <ertyurk@gmail.com>
2024-03-01 16:23:49 +0100

    update lualine with gruvbox

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,6 +6,11 @@ return {
       let g:gruvbox_contrast_dark = 'hard'
       let g:gruvbox_contrast_light = 'hard'
       let g:gruvbox_italic = 1
+      let g:gruvbox_bold = 1
+      let g:italic_comments = 1
+      let g:italic_operator = 1
+      let g:italic_emphasis = 1
+      let g:gruvbox_emphasis = 'bold'
       colorscheme gruvbox
       ]]
     end
